ede6fb6e1faa4224478db6d3017d56f6d4ff72f8
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2025-01-19 00:50:45 +0700

    feat: Server for 3D Object Fetch

---
+++ Server/server.py
@@ -0,0 +1,151 @@
+import paramiko
+from scp import SCPClient
+import os
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+app = FastAPI()
+
+class PromtRequest(BaseModel):
+    prompt: str
+
+class Model3D():
+    model: bytes
+    texture: str
+    mtl: bytes
+
+    def __init__(self, model: bytes, texture: str, mtl: bytes):
+        self.model = model
+        self.texture = texture
+        self.mtl = mtl
+
+# Server details
+hostname = os.getenv("SERVER_HOST")
+username = os.getenv("SERVER_USERNAME")
+password = os.getenv("SERVER_PASSWORD")
+
+remote_folder = os.getenv("REMOTE_PATH")
+local_folder =  os.getenv("LOCAL_PATH") 
+
+def create_scp_client(ssh_client):
+    return SCPClient(ssh_client.get_transport())
+
+def file_to_byte_array(file_path):
+    with open(file_path, "rb") as file:
+        byte_array = file.read()
+    return byte_array
+
+def create_model3D_SSH(promt: str):
+    try:
+        output = None
+        error = None
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, username=username, password=password)
+
+        scp = create_scp_client(client)
+
+        # if os.path.exists(local_folder):
+        #     os.rmdir(local_folder)
+        #     os.makedirs(local_folder)
+
+        # if not os.path.exists(local_folder):
+        #     os.makedirs(local_folder)
+        
+        # activate the virtual environment name myenv
+        command = f"""cd /raid/hvtham/Thesis-Triet-Thanh-k21/Hunyuan3D-1/ && source ~/miniconda3/etc/profile.d/conda.sh && conda activate esroom && python main.py --text_prompt "{promt}" --save_folder ./outputs/test/ --max_faces_num 90000 --do_texture_mapping --do_render"""
+        
+        stdin, stdout, stderr = client.exec_command(command)
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+        print("Output:", output)
+        print("Errors:", error)
+        
+        obj_file = remote_folder + "mesh.obj"
+
+        scp.get(obj_file, local_folder)
+
+        client.close()
+        return {"output": output, "error": error}
+    except Exception as e:
+        print("Error:", str(e))
+        return {"error": str(e)}
+
+def get_model3D_bytes_SSH(promt: str):  
+    result = Model3D(None, None, None)
+
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, username=username, password=password)
+
+        # promt = request.prompt
+        command = f"""cd /raid/hvtham/Thesis-Triet-Thanh-k21/Hunyuan3D-1/ && source ~/miniconda3/etc/profile.d/conda.sh && conda activate esroom && python main.py --text_prompt "{promt}" --save_folder ./outputs/test/ --max_faces_num 90000 --do_texture_mapping --do_render"""
+        
+        stdin, stdout, stderr = client.exec_command(command)
+        print("Output:", stdout.read().decode())
+        print("Errors:", stderr.read().decode())
+
+        model_file = remote_folder + "mesh.obj"
+        mtl_file = remote_folder + "texture.mtl"
+        texture_file = remote_folder + "texture.png"
+
+        print("Model file:", model_file)
+        print("MTL file:", mtl_file)
+        print("Texture file:", texture_file)
+
+        sftp_client = client.open_sftp()
+
+        with sftp_client.file(model_file, mode="rb") as file:
+            result.model = file.read()
+
+        with sftp_client.file(mtl_file, mode="rb") as file:
+            result.mtl = file.read()
+
+        with sftp_client.file(texture_file, mode="rb") as file:
+            texture_byte = file.read()
+
+        result.texture = texture_byte.decode("latin1")
+
+        sftp_client.close()
+        client.close()
+        return result
+    except Exception as e:
+        print("Error:", str(e))
+        return result
+
+@app.post("/create-model3D")
+async def create_model3D(request: PromtRequest):
+    prompt = request.prompt
+
+    result = create_model3D_SSH(prompt)
+
+    if result.get("error"):
+        raise HTTPException(status_code=500, detail=result["error"])
+    
+    return result
+
+@app.post("/get-model3D-bytes")
+async def get_model3D_bytes(request: PromtRequest):
+    prompt = request.prompt
+
+    result = get_model3D_bytes_SSH(request)
+
+    if not result.model:
+        raise HTTPException(status_code=204, detail="No model found")
+    
+    if not result.texture:
+        raise HTTPException(status_code=204, detail="No texture found")
+    
+    if not result.mtl:
+        raise HTTPException(status_code=204, detail="No mtl found")
+    
+    return result
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ Server/test_server.py
@@ -0,0 +1,18 @@
+import requests
+
+# print(requests.get("http://127.0.0.1:8000/").json())
+
+prompt = "a red apple"
+# response = requests.post("http://127.0.0.1:8000/create-model3D", json={"prompt": prompt})
+response = requests.post("http://127.0.0.1:8000/get-model3D-bytes", json={"prompt": prompt})
+
+response.raise_for_status()
+
+image = response.json()["texture"].encode("latin1")
+with open("texture.png", "wb") as file:
+    file.write(image)
+
+if response.status_code != 204:
+    print(response.json())
+else:
+    print("No response")
