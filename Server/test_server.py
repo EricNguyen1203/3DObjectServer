f4e55c2c98bdb088b0bcf2fac592b49288b5e1e8
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2025-02-05 15:34:34 +0700

    feat: change API

--- Server/test_server.py
+++ Server/test_server.py
@@ -2,17 +2,17 @@ import requests
 
 # print(requests.get("http://127.0.0.1:8000/").json())
 
-prompt = "a red apple"
+prompt = "a rainy sky"
 # response = requests.post("http://127.0.0.1:8000/create-model3D", json={"prompt": prompt})
 response = requests.post("http://127.0.0.1:8000/get-model3D-bytes", json={"prompt": prompt})
+# response = requests.post("http://13.236.5.12/get-model3D-bytes", json={"prompt": prompt})
 
 response.raise_for_status()
 
-image = response.json()["texture"].encode("latin1")
-with open("texture.png", "wb") as file:
-    file.write(image)
-
 if response.status_code != 204:
     print(response.json())
+    image = response.json()["texture"].encode("latin1")
+    with open("texture.png", "wb") as file:
+        file.write(image)
 else:
     print("No response")
