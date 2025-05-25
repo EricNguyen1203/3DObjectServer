102a7d87a513c371e1364bd0841f4d2d3cd0ffe4
viettham1998 <viettham1998@gmail.com>
2025-05-25 08:28:18 +0100

    feat: add image 360 apis

--- Server/Routers/Image360Router.py
+++ Server/Routers/Image360Router.py
@@ -28,6 +28,23 @@ def streaming_image_360(prompt: str, title: str):
             ) + "\n"
 
 
+def get_image_360_grpc(path: str):
+    try:
+        with grpc.insecure_channel("localhost:50081") as channel:
+            stub = image_360_pb2_grpc.Image360ServiceStub(channel)
+            request = image_360_pb2.GetImage360Request(path=path)
+            responses = stub.Get360Image(request)
+            for response in responses:
+                if response.status == 0:
+                    raise Exception("Failed to retrieve image")
+                elif response.status in [1, 2]:  # Data chunk or finished
+                    if response.data:
+                        yield response.data
+    except Exception as e:
+        print(f"[grpc_image_stream] Exception: {str(e)}")
+        raise
+
+
 @router.post("/create-image-360")
 async def create_image_360(req: CreateImage360Request):
     try:
@@ -35,6 +52,20 @@ async def create_image_360(req: CreateImage360Request):
         return StreamingResponse(
             streaming_image_360(prompt=req.prompt, title=req.title),
             media_type="application/json",
+        )
+
+    except Exception as e:
+        print(f"Exception: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/get-image-360")
+async def get_image_360(path: str):
+    try:
+        # Connect to gRPC server
+        return StreamingResponse(
+            get_image_360_grpc(path=path),
+            media_type="image/png",
         )
 
     except Exception as e:
